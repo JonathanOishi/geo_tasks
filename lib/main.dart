3832f684fb39188b6dd0a7049d752ef03bfc9504
Jonathan Oishi <jonathan_oishi@hotmail.com>
2026-05-30 17:15:23 -0300

    Ajustando o projeto para fazer o login

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:geo_tasks/app/router/app_routes.dart';
 import 'package:geo_tasks/app/theme/app_theme.dart';
 import 'package:provider/provider.dart';
 import 'package:geo_tasks/features/tasks/viewmodels/tasks_view_model.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'features/tasks/services/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const GeoTasksApp());
 }
 
